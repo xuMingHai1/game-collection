9533d7ca58a8e7acf8f2f5ddb3897ff41f055811
徐明海 <173535609@qq.com>
2025-05-12 17:36:11 +0800

    抽离音效代码

--- tetris/src/main/java/xyz/xuminghai/tetris/game/AudioManager.java
+++ tetris/src/main/java/xyz/xuminghai/tetris/game/AudioManager.java
@@ -626,473 +626,114 @@
 
 package xyz.xuminghai.tetris.game;
 
-import javafx.application.Platform;
-import javafx.beans.property.*;
-import javafx.scene.input.KeyCode;
-import javafx.scene.robot.Robot;
-import xyz.xuminghai.tetris.core.Cell;
-import xyz.xuminghai.tetris.core.Tetris;
-import xyz.xuminghai.tetris.core.TetrisFactory;
+import javafx.scene.media.AudioClip;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
-import java.time.Duration;
-import java.util.*;
-import java.util.function.Function;
-import java.util.stream.Collectors;
+import java.util.Objects;
 
 /**
- * 2024/2/1 21:30 星期四<br/>
- * 游戏规则和数据内容
+ * 2025/5/12 15:19 星期一<br/>
+ * 音频管理
  *
  * @author xuMingHai
  */
-public final class GameWorld {
+final class AudioManager {
 
-    /**
-     * 清除单元格
-     */
-    final ObjectProperty<List<Cell>> clearCell = new SimpleObjectProperty<>(this, "clearCell");
-    /**
-     * 渲染单元格
-     */
-    final ObjectProperty<List<Cell>> renderCell = new SimpleObjectProperty<>(this, "renderCell");
-
-    /**
-     * 消掉的行数
-     */
-    final IntegerProperty lines = new SimpleIntegerProperty(this, "lines") {
-        @Override
-        protected void invalidated() {
-            // 每消除4行进行升级
-            final int newLevel = customizeLevel + super.get() / 4;
-            if (newLevel > level.get()) {
-                AudioManager.getLevelUpAudioClip().play();
-                level.set(newLevel);
-            }
-        }
-    };
-
-    /**
-     * 自定义等级
-     */
-    int customizeLevel;
-
-    private final int maxCustomizeLevel = 30;
-
-    /**
-     * 等级
-     */
-    final IntegerProperty level = new SimpleIntegerProperty(this, "level") {
-
-        @Override
-        protected void invalidated() {
-            if (get() < maxCustomizeLevel) {
-                gameTimeLine.setPulse(GameTimeLine.DEFAULT_PULSE - super.get() * 14);
-            }
-        }
-    };
-
-    /**
-     * 分数
-     */
-    final IntegerProperty score = new SimpleIntegerProperty(this, "score");
-
-    /**
-     * 语言
-     */
-    private final ObjectProperty<Locale> language = new SimpleObjectProperty<>(this, "language", Locale.getDefault());
-
-    private final int rows = 20, cols = 10;
-
-    /**
-     * 游戏数据
-     */
-    final Cell[][] data = new Cell[rows][cols];
-
-    final GameTimeLine gameTimeLine = new GameTimeLine(new Runnable() {
-
-        @Override
-        public void run() {
-            // 初始化当前方块
-            if (currentTetris.get() == null) {
-                currentTetris.set(nextTetris.get());
-            }
-            final Tetris tetris = currentTetris.get();
-            final Cell[] cells = checkCellsRowConvert(tetris.downMove());
-            final Cell[] lastCells = currentCells.get();
-            // 清除上次保存的数据
-            Optional.ofNullable(lastCells)
-                    .ifPresent(GameWorld.this::clearLastCells);
-            if (saveCellsData(cells)) {
-                currentCells.set(checkCellsRowConvert(tetris.copy()));
-            }
-            // 添加失败
-            else {
-                // 是否到顶判断
-                if (lastCells == null || lastCells.length < 4) {
-                    currentTetris.set(null);
-                    currentCells.set(null);
-                    // 游戏时间记录关闭
-                    gameTimeLine.setGameTimeRecord(false);
-                    // 游戏结束动画
-                    gameTimeLine.setGameAnimation(new GameOverAnimation(GameWorld.this));
-                    AudioManager.getBgmMediaPlayer().stop();
-                    AudioManager.getGameOverAudioClip().play();
-                }
-                else {
-                    // 保存上次方块数据
-                    saveCellsData(lastCells);
-                    // 下落失败，清除当前方块，生成新的方块
-                    currentTetris.set(null);
-                    currentCells.set(null);
-                    // 判断是否可以消行
-                    final List<Cell[]> removeRowList = getRemoveRow(lastCells);
-                    final int size = removeRowList.size();
-                    if (size != 0) {
-                        gameTimeLine.setGameAnimation(new RemoveRowsAnimation(GameWorld.this, removeRowList));
-                        lines.set(lines.get() + size);
-                        calculateScore(size);
-                        AudioManager.getClearRowAudioClip().play();
-                    }
-                    else {
-                        // 方块锁定动画
-                        gameTimeLine.setGameAnimation(new TetrisLockAnimation(GameWorld.this, lastCells));
-                    }
-                }
-            }
-
-        }
-
-        /**
-         * 计算分数
-         *
-         * @param size 消除行数
-         */
-        private void calculateScore(int size) {
-            int initScore = switch (size) {
-                case 1 -> 10;
-                case 2 -> 30;
-                case 3 -> 60;
-                case 4 -> 100;
-                default -> throw new IllegalStateException("Unexpected value: " + size);
-            };
-            score.set(score.get() + Math.max(1, level.get()) * initScore);
-        }
-
-
-        private List<Cell[]> getRemoveRow(Cell[] cells) {
-            // 获取可消除的行
-            final Set<Integer> collect = Arrays.stream(cells).map(Cell::getRow).collect(Collectors.toSet());
-            List<Cell[]> list = new LinkedList<>();
-            for (Integer index : collect) {
-                final Cell[] dataRow = data[index];
-                // 判断是否是可消除的行
-                boolean clearable = true;
-                for (Cell c : dataRow) {
-                    if (c == null) {
-                        clearable = false;
-                        break;
-                    }
-                }
-                if (clearable) {
-                    list.add(dataRow);
-                }
-            }
-            return list;
-        }
-
-    });
-
-    /**
-     * 下一个方块
-     */
-    private final ObjectProperty<Tetris> nextTetris = new SimpleObjectProperty<>(this, "nextTetris", TetrisFactory.randomCreateTetris());
-
-    /**
-     * 当前单元格列表
-     */
-    private final ObjectProperty<Cell[]> currentCells = new SimpleObjectProperty<>(this, "currentCells");
-
-    /**
-     * 当前方块
-     */
-    private final ObjectProperty<Tetris> currentTetris = new SimpleObjectProperty<>(this, "currentTetris") {
-        @Override
-        protected void invalidated() {
-            if (super.get() != null) {
-                nextTetris.set(TetrisFactory.randomCreateTetris());
-            }
-        }
-    };
-
-    /**
-     * 游戏运行状态
-     */
-    private boolean gameActive;
-    private Robot robot;
-
-    public boolean getGameActive() {
-        return gameActive;
-    }
-
-    private Robot getRobot() {
-        if (robot == null) {
-            robot = new Robot();
-        }
-        return robot;
-    }
-
-    public int getRows() {
-        return rows;
-    }
-
-    public int getCols() {
-        return cols;
-    }
-
-    public ReadOnlyObjectProperty<Tetris> nextTetrisProperty() {
-        return nextTetris;
-    }
-
-    public ReadOnlyObjectProperty<Cell[]> currentCellsProperty() {
-        return currentCells;
-    }
-
-    public ReadOnlyIntegerProperty linesProperty() {
-        return lines;
-    }
-
-    public ReadOnlyIntegerProperty scoreProperty() {
-        return score;
-    }
-
-    public ReadOnlyIntegerProperty levelProperty() {
-        return level;
-    }
-
-    public ReadOnlyObjectProperty<Locale> languageProperty() {
-        return language;
-    }
-
-    public ReadOnlyObjectProperty<Duration> gameTimeProperty() {
-        return gameTimeLine.gameTimeProperty();
-    }
-
-    public ReadOnlyObjectProperty<List<Cell>> clearCellProperty() {
-        return clearCell;
-    }
-
-    public ReadOnlyObjectProperty<List<Cell>> renderCellProperty() {
-        return renderCell;
+    private static final class BgmMediaPlayerHolder {
+        private static final MediaPlayer BGM_MEDIA_PLAYER = new MediaPlayer(new Media(Objects.requireNonNull(AudioManager.class.getResource("/audio/bgm.mp3")).toExternalForm()));
     }
 
     /**
-     * 清除上一次单元格列表数据
+     * 懒加载的背景音乐播放器
      *
-     * @param cells 单元格数组
+     * @return BgmMediaPlayer
      */
-    private void clearLastCells(Cell[] cells) {
-        for (Cell cell : cells) {
-            data[cell.getRow()][cell.getCol()] = null;
-        }
+    public static MediaPlayer getBgmMediaPlayer() {
+        return BgmMediaPlayerHolder.BGM_MEDIA_PLAYER;
+    }
+
+    private static final class ClearRowAudioClipHolder {
+        private static final AudioClip CLEAR_ROW_AUDIO_CLIP = new AudioClip(Objects.requireNonNull(AudioManager.class.getResource("/audio/clear_rows.wav")).toExternalForm());
     }
 
     /**
-     * 检查单元格行是否超出索引并转换
+     * 懒加载的消除行音效
      *
-     * @param cells 单元格数组
-     * @return 原始单元格或转换后的
+     * @return ClearRowAudioClip
      */
-    private Cell[] checkCellsRowConvert(Cell[] cells) {
-        // 是否需要转换
-        boolean convertable = false;
-        for (Cell cell : cells) {
-            if (cell.getRow() < 0) {
-                // 未显示的方块，如果列不符合
-                if (cell.getCol() < 0 || cell.getCol() >= getCols()) {
-                    return new Cell[0];
-                }
-                convertable = true;
-            }
-        }
-        // 需要转换
-        if (convertable) {
-            List<Cell> list = new LinkedList<>();
-            for (Cell cell : cells) {
-                if (cell.getRow() >= 0) {
-                    list.add(cell);
-                }
-            }
-            return list.toArray(new Cell[0]);
-        }
-        // 返回原始数组
-        return cells;
+    public static AudioClip getClearRowAudioClip() {
+        return ClearRowAudioClipHolder.CLEAR_ROW_AUDIO_CLIP;
+    }
+
+
+    private static final class MoveAudioClipHolder {
+        private static final AudioClip MOVE_AUDIO_CLIP = new AudioClip(Objects.requireNonNull(AudioManager.class.getResource("/audio/move.wav")).toExternalForm());
     }
 
     /**
-     * 规则检测并设置数据
+     * 懒加载的方块移动音效
      *
-     * @param cells 单元格列表
-     * @return 是否保存数据成功
+     * @return MoveAudioClip
      */
-    private boolean saveCellsData(Cell[] cells) {
-        if (cells.length == 0) {
-            return false;
-        }
-        for (Cell cell : cells) {
-            final int row = cell.getRow();
-            final int col = cell.getCol();
-            // 是否超出范围或被占用
-            if (row >= rows || col < 0 || col >= cols || data[row][col] != null) {
-                return false;
-            }
-        }
-
-        // 保存数据
-        for (Cell cell : cells) {
-            final int row = cell.getRow();
-            final int col = cell.getCol();
-            data[row][col] = cell;
-        }
-        return true;
+    public static AudioClip getMoveAudioClip() {
+        return MoveAudioClipHolder.MOVE_AUDIO_CLIP;
     }
 
+
+    private static final class RotateAudioClipHolder {
+        private static final AudioClip ROTATE_AUDIO_CLIP = new AudioClip(Objects.requireNonNull(AudioManager.class.getResource("/audio/rotate.wav")).toExternalForm());
+    }
 
     /**
-     * 启动或暂停游戏
-     */
-    public void startOrPauseGame() {
-        final Optional<Boolean> keyLocked = Platform.isKeyLocked(KeyCode.CAPS);
-        if (gameActive) {
-            keyLocked.ifPresent(b -> {
-                if (b) {
-                    getRobot().keyType(KeyCode.CAPS);
-                }
-            });
-            AudioManager.getBgmMediaPlayer().pause();
-            gameTimeLine.stop();
-        }
-        else {
-            // 大写锁定处理
-            keyLocked.ifPresent(b -> {
-                if (!b) {
-                    getRobot().keyType(KeyCode.CAPS);
-                }
-            });
-            AudioManager.getBgmMediaPlayer().play();
-            gameTimeLine.start();
-        }
-        gameActive = !gameActive;
-    }
-
-    private enum ActionEnum {
-        DOWN_MOVE,
-        LEIF_MOVE,
-        RIGHT_MOVE,
-        ROTATE_CLOCKWISE,
-        ROTATE_COUNTER_CLOCKWISE
-    }
-
-
-    /**
-     * 方块行为
+     * 懒加载的方块旋转音效
      *
-     * @param action   行为
-     * @param function 行为函数
+     * @return RotateAudioClip
      */
-    private void tetrisAction(ActionEnum action, Function<Tetris, Cell[]> function) {
-        final Tetris tetris = currentTetris.get();
-        if (tetris != null) {
-            final Cell[] copyCells = tetris.copy();
-            final Cell[] cells = checkCellsRowConvert(function.apply(tetris));
-            // 不相同时执行
-            if (!Arrays.equals(copyCells, cells)) {
-                // 清除上次保存的数据
-                clearLastCells(currentCells.get());
-                if (saveCellsData(cells)) {
-                    switch (action) {
-                        case DOWN_MOVE, LEIF_MOVE, RIGHT_MOVE -> AudioManager.getMoveAudioClip().play();
-                        case ROTATE_CLOCKWISE, ROTATE_COUNTER_CLOCKWISE -> AudioManager.getRotateAudioClip().play();
-                    }
-                    currentCells.set(checkCellsRowConvert(tetris.copy()));
-                }
-                else {
-                    tetris.setCells(copyCells);
-                }
-            }
-        }
+    public static AudioClip getRotateAudioClip() {
+        return RotateAudioClipHolder.ROTATE_AUDIO_CLIP;
+    }
+
+
+    private static final class LevelUpAudioClipHolder {
+        private static final AudioClip LEVEL_UP_AUDIO_CLIP = new AudioClip(Objects.requireNonNull(AudioManager.class.getResource("/audio/level_up.wav")).toExternalForm());
     }
 
     /**
-     * 向下移动
+     * 懒加载的升级音效
+     *
+     * @return LevelUpAudioClip
      */
-    void downMove() {
-        tetrisAction(ActionEnum.DOWN_MOVE, Tetris::downMove);
+    public static AudioClip getLevelUpAudioClip() {
+        return LevelUpAudioClipHolder.LEVEL_UP_AUDIO_CLIP;
+    }
+
+
+    private static final class LevelDownAudioClipHolder {
+        private static final AudioClip LEVEL_DOWN_AUDIO_CLIP = new AudioClip(Objects.requireNonNull(AudioManager.class.getResource("/audio/level_down.wav")).toExternalForm());
     }
 
     /**
-     * 向左移动
+     * 懒加载的降级音效
+     *
+     * @return LevelDownAudioClip
      */
-    void leftMove() {
-        tetrisAction(ActionEnum.LEIF_MOVE, Tetris::leftMove);
+    public static AudioClip getLevelDownAudioClip() {
+        return LevelDownAudioClipHolder.LEVEL_DOWN_AUDIO_CLIP;
+    }
+
+
+    private static final class GameOverAudioClipHolder {
+        private static final AudioClip GAME_OVER_AUDIO_CLIP = new AudioClip(Objects.requireNonNull(AudioManager.class.getResource("/audio/game_over.wav")).toExternalForm());
     }
 
     /**
-     * 向右移动
+     * 懒加载的游戏结束音效
+     *
+     * @return GameOverAudioClip
      */
-    void rightMove() {
-        tetrisAction(ActionEnum.RIGHT_MOVE, Tetris::rightMove);
-    }
-
-    /**
-     * 顺时针旋转
-     */
-    public void rotateClockwise() {
-        tetrisAction(ActionEnum.ROTATE_CLOCKWISE, Tetris::rotateClockwise);
-    }
-
-    /**
-     * 逆时针旋转
-     */
-    public void rotateCounterClockwise() {
-        tetrisAction(ActionEnum.ROTATE_COUNTER_CLOCKWISE, Tetris::rotateCounterClockwise);
-    }
-
-    /*
-     **********************游戏行为和规则************************
-     */
-
-    /**
-     * 等级加1
-     */
-    public void levelPlus() {
-        if (customizeLevel < maxCustomizeLevel) {
-            AudioManager.getLevelUpAudioClip().play();
-            final int levelValue = level.get() - customizeLevel++;
-            level.set(customizeLevel + levelValue);
-        }
-    }
-
-    /**
-     * 等级减1
-     */
-    public void levelMinus() {
-        if (customizeLevel > 0) {
-            AudioManager.getLevelDownAudioClip().play();
-            final int levelValue = level.get() - customizeLevel--;
-            level.set(customizeLevel + levelValue);
-        }
-    }
-
-    /**
-     * 切换语言
-     */
-    public void switchLanguage() {
-        if (Locale.ENGLISH.equals(language.get())) {
-            language.set(Locale.CHINA);
-        }
-        else {
-            language.set(Locale.ENGLISH);
-        }
+    public static AudioClip getGameOverAudioClip() {
+        return GameOverAudioClipHolder.GAME_OVER_AUDIO_CLIP;
     }
 
 }
